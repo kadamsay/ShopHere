7d825623dfb97b94bf2da546ac4f429abd7f7f74
kadamsay <37590027+kadamsay@users.noreply.github.com>
2018-04-08 16:00:39 +0530

    Add files via upload

---
+++ kid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kid',
+  templateUrl: './kid.component.html',
+  styleUrls: ['./kid.component.scss']
+})
+export class KidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
